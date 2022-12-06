53aad9b4e77d0ae8a65be078d94e2b710cbc6d95
AgDelfino <agustin.delfino92@gmail.com>
2022-12-05 22:18:52 -0300

    form buttons controlled

--- PI-Pokemon-main/client/src/components/Create/Create.jsx
+++ PI-Pokemon-main/client/src/components/Create/Create.jsx
@@ -21,13 +21,43 @@ const Create = () => {
     speed: 0,
     height: 0,
     weight: 0,
-    image: "",
-    types: [],
+    types: ["",""],
   });
+
+  useEffect(() => {
+    console.log(input.types);
+  },[input])
+
   const handleChange = (e) => {
-    setInput({...input, [e.target.name]: e.target.value})
-    console.log(input);
+    setInput({ ...input, [e.target.name]: e.target.value });
   };
+
+  const handleTypesChange = (e) => {
+    e.preventDefault()
+    let type = e.target.name
+    let newType = [...input.types]
+
+    if(input.types.find(t => t === type)){
+      return setInput((state) => ({...state, types: input.types.filter(t => t !== type).concat('')}))
+    }
+
+    if(input.types[0] && input.types[1]){
+      return setInput((state) => ({...state, types: newType}))
+    }
+    if(!input.types[0] && !input.types[1]) {
+      newType[0] = type
+      return setInput((state) => ({...state, types: newType}))
+    }
+    if(input.types[0] && !input.types[1]){
+      newType[1] = type
+      return setInput((state) => ({...state, types: newType}))
+    }
+    if(!input.types[0] && input.types[1]){
+      newType[0] = type
+      return setInput((state) => ({...state, types: newType}))
+    }
+
+  }
 
   return (
     <div>
@@ -35,26 +65,62 @@ const Create = () => {
       <h1>CREATE YOUR POKEMON!</h1>
 
       <div className={styles.form_container}>
-        <form className={styles.form} >
+        <form className={styles.form}>
           <input
+            value={input.name}
+            name="name"
             type="text"
             placeholder="Name..."
             onChange={(e) => handleChange(e)}
           ></input>
-          <input type="number" placeholder="Attack..."  onChange={(e) => handleChange(e)} ></input>
-          <input type="number" placeholder="Defense..."></input>
-          <input type="number" placeholder="HP..."></input>
-          <input type="number" placeholder="Speed..."></input>
-          <input type="number" placeholder="Height..."></input>
-          <input type="number" placeholder="Weight..."></input>
-          <input type="text" placeholder="URL..."></input>
+          <input
+            value={input.attack}
+            name="attack"
+            type="number"
+            placeholder="Attack..."
+            onChange={(e) => handleChange(e)}
+          ></input>
+          <input
+            value={input.defense}
+            name="defense"
+            type="number"
+            placeholder="Defense..."
+            onChange={(e) => handleChange(e)}
+          ></input>
+          <input
+            value={input.hp}
+            name="hp"
+            type="number"
+            placeholder="HP..."
+            onChange={(e) => handleChange(e)}
+          ></input>
+          <input
+            value={input.speed}
+            name="speed"
+            type="number"
+            placeholder="Speed..."
+            onChange={(e) => handleChange(e)}
+          ></input>
+          <input
+            value={input.height}
+            name="height"
+            type="number"
+            placeholder="Height..."
+            onChange={(e) => handleChange(e)}
+          ></input>
+          <input
+            value={input.weight}
+            name="weight"
+            type="number"
+            placeholder="Weight..."
+            onChange={(e) => handleChange(e)}
+          ></input>
           {types && types.length
             ? types.map((type) => {
-                return <button value={type.name}>{type.name}</button>;
+                return <button  name={type.name} value={type.name} onClick={handleTypesChange} key={type.name}>{type.name}</button>;
               })
             : null}
         </form>
-        {/* <Pokemon id={""} name={""} type /> */}
       </div>
     </div>
   );
